c0b025c1b412c6951347c3904b73764d2c0fee03
JeonSuHwan <jsuhwan34@gmail.com>
2019-10-14 21:21:31 +0900

    Modified code (1)

--- ink.c
+++ ink.c
@@ -1,33 +1,100 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 typedef struct dot {
 	double x;
 	double y;
+	int check;
 	double weight;
+	struct weight *w;
 }dot;
+
+struct weight {
+	int index;
+	double weight;
+};
+
+int checkbox(dot* d, int num)
+{
+	int sum = 0;
+	for (int i = 0; i < num; i++)
+	{
+		sum += d[i].check;
+	}
+	if (sum == num)
+		return 1;
+}
 
 void main()
 {
 	int dots;
 	dot* d;
+	/*double weight = 0;*/
+	double total = 0;
+	double min;
+	double weight = 0;
 	scanf("%d", &dots);
 	d = (dot*)malloc(sizeof(dot) * dots);
+	for (int i = 0; i < dots; i++)
+	{
+		d[i].w = (struct weight*)malloc(sizeof(struct weight)*(dots-1));
+	}
 
 	for (int i = 0; i < dots; i++) // Read the vertexes
 	{
-		scanf("%f %f", &d[i].x, &d[i].y);
+		scanf("%lf %lf", &d[i].x, &d[i].y);
+		d[i].check = 0;
 	}
 	
-	/*for (int i = 0; i < dots-1 i++)
+	for (int i = 0; i < dots; i++)
 	{
-		int j = 1;
-		for (j; j < dots; j++)
+		int h = 0;
+		for (int j = 0; j < dots; j++)
 		{
-
+			if (i!=j)
+			{
+				d[i].w[h].index = j;
+				d[i].w[h].weight = sqrt(pow(d[i].x - d[j].x, 2.0) + pow(d[i].y - d[j].y, 2.0));
+				h++;
+			}
 		}
-		j++;
-	}*/
+	}
 	//use Prim algorithm.
+	
+	int check = 0;
+	int k = 0;
+	int minIndex = 0;
+	d[0].weight = 0;
+	d[0].check = 1;
+	for(int j=0; j<dots; j++)
+	{
+		min = 100;
+		for (int i = 0; i < dots - 1; i++)
+		{
+			if (d[d[k].w[i].index].check != 1)
+			{
+				if (d[k].w[i].weight < min)
+				{
+					min = d[k].w[i].weight;
+					minIndex = d[k].w[i].index;
+				}
+			}
+		}
+		d[k].weight = min;
+		k = minIndex;
+		d[k].check = 1;
+	} 
+
+	for (int i = 0; i < dots; i++)
+	{
+		total += d[i].weight;
+	}
+	printf("\n%.2f", total-100);
+	for (int i = 0;i < dots; i++)
+	{
+		free(d[i].w);
+	}
 	free(d);
 }
